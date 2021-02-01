9d5531782743c644a1d84e08bfb6d78c7d9c2cfe
christophorBlagoev <christophor.blagoev@gmail.com>
2021-02-01 12:44:41 +0200

    Initial commit

--- src/L01_StacksAndQueues/E07_RecursiveFibonacci.java
+++ src/L01_StacksAndQueues/E07_RecursiveFibonacci.java
@@ -1,4 +1,38 @@
 package L01_StacksAndQueues;
 
+import java.util.Scanner;
+
 public class E07_RecursiveFibonacci {
+    private static long[] memo;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        memo = new long[n + 1];
+
+        System.out.println(recursiveFibonacci(n));
+
+    }
+
+   /* private static long recursiveFibonacci(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+    }*/
+
+    private static long recursiveFibonacci(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        memo[n] = recursiveFibonacci(n-1) + recursiveFibonacci(n - 2);
+        return memo [n];
+    }
 }
